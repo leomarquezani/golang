d65a8e0bfe6e815303205e98c08ea0d01a13d421
Leo <leomarquezani@gmail.com>
2018-08-21 20:14:11 -0300

    Added Mux Router

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"log"
 )
 
 func main(){
-	fmt.Println("Hello")
+
+	router := mux.NewRouter()
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
