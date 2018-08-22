4775cfe5157c2d602067496c294e1fa2a1c9cd94
Leo <leomarquezani@gmail.com>
2018-08-21 22:01:08 -0300

    Added new methods

--- main.go
+++ main.go
@@ -29,11 +29,34 @@ func GetPeople(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
-func GetPerson(w http.ResponseWriter, r *http.Request) {}
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+}
 
-func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.Id = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
 
-func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.Id == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
 
 type Person struct {
 	Id        string   `json:"id,omitempty"`
